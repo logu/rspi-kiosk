fbaaa5ddfbc1b9c3f70f3d27da935b1081880352
Logu Kirou <Logu@MacBook-Air-de-Logu.local>
2016-11-22 14:12:41 +0100

    disabled background to X

--- tools/docker/dockerfiles/x11/x.sh
+++ tools/docker/dockerfiles/x11/x.sh
@@ -5,4 +5,4 @@ LOCKFILE=/tmp/.X0-lock
 # remove lockfile before launching X.
 
 rm -f $LOCKFILE ;
-X -s 0 -quiet -dpms -nocursor -novtswitch vt2
+X -s 0 -quiet -dpms -nocursor -novtswitch vt2 -background none
